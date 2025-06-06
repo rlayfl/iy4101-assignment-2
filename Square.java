a6628a9c74aee0f55d3f3b0e974d431e9265348b
Richard Lay-Flurrie <rlayfl@essex.ac.uk>
2025-06-06 15:14:21 +0100

    Call to super in Square

--- Square.java
+++ Square.java
@@ -1,10 +1,10 @@
-public class Square {
+public class Square extends Shape {
     
     private int sideLength;
 
     public Square(int sideLength) {
+        super(4, new Coordinates(0, 0));
         this.sideLength = sideLength;
-        
     }
 
     public int getSideLength() {
